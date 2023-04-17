9cbc671bee37c6ac981d8fc9381c6462ffca605b
lia0wang <lia0wang@outlook.com>
2023-04-18 01:52:33 +1000

    add enter session nav button when user is logged out

--- frontend/src/page/DashboardPage.tsx
+++ frontend/src/page/DashboardPage.tsx
@@ -1,12 +1,22 @@
-import { Grid } from '@mui/material';
+import { Grid, Input } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import Navbar from '../component/dashboard/Navbar';
 import GameCard from '../component/dashboard/GameCard';
 import apiRequest from '../util/api';
+import { useNavigate } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
 
 const DashboardPage: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [sessionId, setSessionId] = useState(null);
+  const [showNewGameModal, setShowNewGameModal] = useState(true);
+
+  const navigate = useNavigate();
+  const goToSessionAdminPage = () => {
+    navigate(`/session/${sessionId}`);
+  };
+
   useEffect(() => {
     if (localStorage.getItem('token')) {
       setIsLoggedIn(true);
@@ -46,8 +56,40 @@ const DashboardPage: React.FC = () => {
             </Grid>
           </Box>
         </div>
-      </>)
-        : (<> </>)}
+      </>
+          )
+        : (
+            <>
+            {showNewGameModal && (
+              <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white p-8 rounded-md shadow-lg w-[370px]">
+                <div className="flex flex-row justify-between">
+                  <h2 className="text-2xl mb-4 text-center">Input Session ID</h2>
+                  <CloseIcon className="cursor-pointer" onClick={() => setShowNewGameModal(false)} />
+                </div>
+                <div className="bg-gray-100 p-2 rounded-lg mb-4 flex justify-center">
+                  <Input className="w-[300px]" type="text" onChange={(e) => setSessionId(e.target.value)} />
+                </div>
+                <div className="flex flex-row justify-around mt-2">
+                  <button
+                    className="bg-yellow-500 hover:bg-yellow-400 text-black font-bold py-2 px-4 w-[300px]
+                                border-b-4 border-yellow-700 hover:border-yellow-500 rounded"
+                    type="button"
+                    onClick={goToSessionAdminPage}>
+                    GO TO SESSION PAGE
+                  </button>
+                    {/* className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 w-[70px]
+                                  border border-gray-400 rounded shadow"
+                    type="button">
+                    {/* // onClick={{onClose}}> */}
+                    {/* Close
+                  </button> */}
+                </div>
+                </div>
+              </div>
+            )}
+          </>
+          )}
     </>
   );
 };
